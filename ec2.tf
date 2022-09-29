ef3d159eeef09a2e7143ffde1c337420725a87a0
mehul1215 <54547372+mehul1215@users.noreply.github.com>
2022-09-29 13:59:49 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "web" {
+    ami = "ami-05fa00d4c63e32376"
+    instance_type = "t2.micro"
+    count = 1
+    key_name = "Jenkins-Key"
+    tags = {
+      "Name" = "web"
+    }
+  
+}
